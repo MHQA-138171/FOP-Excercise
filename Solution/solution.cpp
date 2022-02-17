d16d0e0b86049f7d2e97c7bedc317fcbf1c15ce9
MHQA-138171 <99812198+MHQA-138171@users.noreply.github.com>
2022-02-17 17:58:05 +0500

    Add files via upload

---
+++ Solution/solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Student
+{
+private:
+    float marks;
+    string name;
+
+public:
+    void input(string n, float m)
+    {
+        cout << "Enetr your name: ";
+        cin >> n;
+        name = n;
+        cout << "Enter your marks: ";
+        cin >> m;
+        marks = m;
+    }
+    void display()
+    {
+        cout << "Student: " << name << endl
+             << "Marks: " << marks;
+    }
+};
+
+int main()
+{
+    Student S;
+    float marks;
+    string name;
+    S.input(name, marks);
+    S.display();
+}
